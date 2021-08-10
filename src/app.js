5373661e134edae52435d32ed483a7b1821b826e
ChiaraR3 <chiara.r.3@live.it>
2021-08-10 16:04:56 +0000

    terzo

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 /* eslint-disable */
-import "bootstrap";
+
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
+  let card = document.querySelector(".card");
+  card.classList.add(generateRandomSuit());
+  card.innerHTML = generateRandomNumber();
   console.log("tutto bene!");
 };
+
+function generateRandomNumber() {
+  let numbers = [
+    "A",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let number = Math.floor(Math.random() * numbers.length);
+  return numbers[number];
+}
+
+function generateRandomSuit() {
+  let suits = ["spade", "club", "heart", "diamond"];
+  let suit = Math.floor(Math.random() * suits.length);
+  return suits[suit];
+}
